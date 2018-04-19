7ad7ecd29f1fe6aba0d77eb1e6dcab8b19df3a25
Gabi Aizenman <lucky3gabi@gmail.com>
2018-04-19 11:01:47 -0700

    lol

--- Unit17/src/Ammo.java
+++ Unit17/src/Ammo.java
@@ -6,52 +6,43 @@
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Image;
-import java.io.File;
-import javax.imageio.ImageIO;
 
-public class Ammo extends MovingThing
-{
+public class Ammo extends MovingThing {
 	private int speed;
 
-	public Ammo()
-	{
-		this(0,0,0);
+	public Ammo() {
+		this(0, 0, 5);
 	}
 
-	public Ammo(int x, int y)
-	{
-		//add code
+	public Ammo(int x, int y) {
+		this(x, y, 5);
 	}
 
-	public Ammo(int x, int y, int s)
-	{
-		//add code
+	public Ammo(int x, int y, int s) {
+		setPos(x, y);
+		setSpeed(s);
 	}
 
-	public void setSpeed(int s)
-	{
-	   //add code
+	public void setSpeed(int s) {
+		speed = s;
 	}
 
-	public int getSpeed()
-	{
-	   return 0;
+	public int getSpeed() {
+		return speed;
 	}
 
-	public void draw( Graphics window )
-	{
-		//add code to draw the ammo
-	}
-	
-	
-	public void move( String direction )
-	{
-		//add code to draw the ammo
+	public void draw(Graphics window) {
+		window.setColor(Color.YELLOW);
+		window.fillRect(getX(), getY(),10, 10);
 	}
 
-	public String toString()
-	{
-		return "";
+	public void move(String direction) {
+		if (direction.equals("UP")) {
+			setY(getY() - speed);
+		}
 	}
-}
+
+	public String toString() {
+		return super.toString() + " " + getSpeed();
+	}
+} 
--- Unit17/src/MovingThing.java
+++ Unit17/src/MovingThing.java
@@ -4,93 +4,88 @@
 //Class - 
 //Lab  -
 
-import java.awt.Color;
 import java.awt.Graphics;
 
-public abstract class MovingThing implements Moveable
-{
+public abstract class MovingThing implements Moveable {
 	private int xPos;
 	private int yPos;
 	private int width;
 	private int height;
 
-	public MovingThing()
-	{
-		xPos = 10;
-		yPos = 10;
-		width = 10;
-		height = 10;
+	public MovingThing() {
+		this(10, 10, 10, 10);
 	}
 
-	public MovingThing(int x, int y)
-	{
-		xPos = x;
-		yPos = y;
-		width = 10;
-		height = 10;
+	public MovingThing(int x, int y) {
+		this(x, y, 10, 10);
 	}
 
-	public MovingThing(int x, int y, int w, int h)
-	{
-		//add code here
-		setX(x);
-		setY(y);
-		setPos(x, y);
+	public MovingThing(int x, int y, int w, int h) {
+		setPos(x - w / 2, y - w / 2);
 		setWidth(w);
 		setHeight(h);
 	}
 
-	public void setPos( int x, int y)
-	{
+	public boolean isCollidingMT(MovingThing mt) {
+		// check collision at central left, right, top, and bottom
+		if (partialCollision(this.getX(), this.getY() + this.getHeight() / 2, mt))
+			return true;
+		else if (partialCollision(this.getX() + this.getWidth(), this.getY() + this.getHeight() / 2, mt))
+			return true;
+		else if (partialCollision(this.getX() + this.getWidth() / 2, this.getY(), mt))
+			return true;
+		else if (partialCollision(this.getX() + this.getWidth() / 2, this.getY() + this.getHeight(), mt))
+			return true;
+		return false;
+	}
+
+	private boolean partialCollision(int pointX, int pointY, MovingThing mt) {
+		return pointX > mt.getX() && pointX < mt.getX() + mt.getWidth() && pointY > mt.getY()
+				&& pointY < mt.getY() + mt.getHeight();
+	}
+
+	public void setPos(int x, int y) {
+		setX(x);
+		setY(y);
+	}
+
+	public void setX(int x) {
 		xPos = x;
+	}
+
+	public void setY(int y) {
 		yPos = y;
 	}
 
-	public void setX(int x)
-	{
-		xPos= x;
+	public int getX() {
+		return xPos;
 	}
 
-	public void setY(int y)
-	{
-		yPos = y;
+	public int getY() {
+		return yPos;
 	}
 
-	public int getX()
-	{
-		return xPos;   //finish this method
+	public void setWidth(int w) {
+		width = w;
 	}
 
-	public int getY()
-	{
-		return yPos;  //finish this method
+	public void setHeight(int h) {
+		height = h;
 	}
 
-	public void setWidth(int w)
-	{
-		width = w;//add code here
+	public int getWidth() {
+		return width;
 	}
 
-	public void setHeight(int h)
-	{
-		height = h;//add code here
-	}
-
-	public int getWidth()
-	{
-		return width;  //finish this method
-	}
-
-	public int getHeight()
-	{
-		return height;  //finish this method
+	public int getHeight() {
+		return height;
 	}
 
 	public abstract void move(String direction);
+
 	public abstract void draw(Graphics window);
 
-	public String toString()
-	{
+	public String toString() {
 		return getX() + " " + getY() + " " + getWidth() + " " + getHeight();
 	}
 }
